6a637bfed89309a87373ede41e77ce2b2dec2fb1
James John McGuire <jamesjohnmcguire@gmail.com>
2026-01-31 15:24:37 +0900

    Add PHPStan Support File

---
+++ SourceCode/stubs/phpstan-bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Notarization Application PlugIn - PHPStan Bootstrap File
+ *
+ * @package   NotarizationApplicationPlugIn
+ * @author    James John McGuire <jamesjohnmcguire@gmail.com>
+ * @copyright 2017 - 2026 James John McGuire <jamesjohnmcguire@gmail.com>
+ * @license   MIT
+ * @link      https://jamesjohnmcguire@bitbucket.org/jamesjohnmcguire/notaryformplugin
+ */
+
+declare(strict_types=1);
+
+if ( false === defined('ABSPATH') )
+{
+	define('ABSPATH', __DIR__ . '/');
+}
