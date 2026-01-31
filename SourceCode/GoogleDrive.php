a6cd08e27573a1da766eb6e4c1d47bb6acbc3e70
James John McGuire <jamesjohnmcguire@gmail.com>
2026-01-31 22:12:42 +0900

    Add File Header Doc Blocks

--- SourceCode/GoogleDrive.php
+++ SourceCode/GoogleDrive.php
@@ -1,4 +1,18 @@
 <?php
+
+/**
+ * Google Drive Command Line Interface
+ *
+ * A command-line tool for interacting with Google Drive.
+ *
+ * @package   GoogleDriveCLI
+ * @author    James John McGuire <jamesjohnmcguire@gmail.com>
+ * @copyright 2021 - 2026 by Digital Zen Works
+ * @license   MIT License
+ * @version   1.3.22
+ * @link      https://github.com/jamesjohnmcguire/GoogleDriveCLI
+ */
+
 declare(strict_types=1);
 
 namespace DigitalZenWorks\GoogleDrive;
